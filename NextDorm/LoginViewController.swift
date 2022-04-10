5082c086da9c8b88d9fc192c9e144ab1dbffe1fd
Daisy Rocha-Montanez <daisy_rocha10@yahoo.com>
2022-04-10 13:00:33 -0700

    email verification
    We can now confirm that the last 9 characters in the email field are "csumb.edu" to ensure we have a CSUMB email address

--- NextDorm/LoginViewController.swift
+++ NextDorm/LoginViewController.swift
@@ -41,11 +41,14 @@ class LoginViewController: UIViewController {
         user.username = usernameField.text
         user.password = passwordField.text
         
-        user.signUpInBackground { (success, error) in
-            if success {
-                self.performSegue(withIdentifier: "loginSegue", sender: nil)
-            } else {
-                print("Error: \(error!.localizedDescription)")
+        let csumb = "csumb.edu"
+        if user.email!.suffix(9) == csumb {
+            user.signUpInBackground { (success, error) in
+                if success {
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                } else {
+                    print("Error: \(error!.localizedDescription)")
+                }
             }
         }
     }
