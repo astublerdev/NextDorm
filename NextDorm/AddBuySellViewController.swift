7117ef60c630ec27c61f1ca3315781b1e6e3f143
Daisy Rocha-Montanez <daisy_rocha10@yahoo.com>
2022-04-29 22:57:18 -0700

    Created missing views
    Still working on it, not completed yet.

---
+++ NextDorm/AddBuySellViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddBuySellViewController.swift
+//  NextDorm
+//
+//  Created by Daisy Rocha on 4/29/22.
+//
+
+import UIKit
+
+class AddBuySellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
