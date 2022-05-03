a6df7a6fba0e882afb967c78adaca3c3a1f59336
Daisy Rocha-Montanez <daisy_rocha10@yahoo.com>
2022-05-03 11:53:20 -0700

    LostAndFound & BuyAndSell Feed isn't loading
    Seems that posts are being saved, according to the console, but we cannot see them in either feed.

--- NextDorm/BuyAndSellViewController.swift
+++ NextDorm/BuyAndSellViewController.swift
@@ -6,24 +6,63 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class BuyAndSellViewController: UIViewController {
+class BuyAndSellViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BuySellCell") as! BuySellCell
+        
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.descriptionLabel.text = post["description"] as? String
+        
+        cell.priceLabel.text = post["price"] as! String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"BuyAndSellPost")
+        query.includeKey("author")
+        query.limit = 100
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
