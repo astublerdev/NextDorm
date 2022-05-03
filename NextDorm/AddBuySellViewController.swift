a08d246d23d26ebab40128d1a2188b5d9651af36
Daisy Rocha-Montanez <daisy_rocha10@yahoo.com>
2022-05-02 22:15:34 -0700

    deleted some files
    Almost finished. Just need to set up the tableViews

--- NextDorm/AddBuySellViewController.swift
+++ NextDorm/AddBuySellViewController.swift
@@ -2,18 +2,81 @@
 //  AddBuySellViewController.swift
 //  NextDorm
 //
-//  Created by Daisy Rocha on 4/29/22.
+//  Created by Daisy Rocha on 5/2/22.
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
-class AddBuySellViewController: UIViewController {
+class AddBuySellViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var descriptionField: UITextField!
+    
+    @IBOutlet weak var priceField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["description"] = descriptionField.text!
+        post["author"] = PFUser.current()!
+        post["price"] = priceField.text!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            } else {
+                print("error!")
+            }
+        }
+    }
+    
+    @IBAction func onCancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to:size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     
 
     /*
